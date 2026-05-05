2ff562884e4abeb408baf97a7bdde6cd4997176f
JKuchnio <kuchnio24156@stud.akademiabialska.pl>
2026-05-05 10:13:09 +0200

    git add . git commit -m "Added file handling service (load/save)"

---
+++ FileService.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+public class FileService
+{
+    public string LoadFile(string path)
+    {
+        return File.ReadAllText(path);
+    }
+
+    public void SaveFile(string path, string content)
+    {
+        File.WriteAllText(path, content);
+    }
+}
